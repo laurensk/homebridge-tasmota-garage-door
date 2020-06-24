dfbe21b3dfc1f769ca881006d849edf9b84947c8
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-06-24 18:04:58 +0200

    added Timeslots feature

--- README.md
+++ README.md
@@ -43,5 +43,34 @@ When your Tasmota Relays are ready and configured correctly, add the following c
 }
 ```
 
+## Timeslots
+You can use Timeslots to prevent simultaneous requests to the relays. Since most remote controls for garage doors doesn't support simultaneous button presses, you can't use them in HomeKit Scenes without further configuration. By using Timeslots in version 2.0.0 and newer, you can enable Timeslots easily.
+
+```json
+{
+  "accessory": "Garage-Door",
+  "name": "Car",
+  "host": "Hostname or IP of Tasmota Relay",
+  "relay": "1",
+  "password": "Password of the Web-Interface",
+  "useTimeslots": true,
+  "timeslotIndex": 1
+},
+{
+  "accessory": "Garage-Door",
+  "name": "Car",
+  "host": "Hostname or IP of Tasmota Relay",
+  "relay": "2",
+  "password": "Password of the Web-Interface",
+  "useTimeslots": true,
+  "timeslotIndex": 2
+}
+```
+
+Please take a closer look at the 'timeslotIndex' property and make sure it's an increasing number starting at 1 for all your garage doors (1, 2, 3, 4, ...).
+This property is used to create an unmessurage delay between single requests in order to prevent simultaneous calls to the Homebridge.
+
+Note that the Timeslots feature won't work if you don't provide the 'timeslotIndex' property for every garage door which has 'useTimeslots' enabled.
+
 ## Thanks
 Thanks to [ageorgios/homebridge-sonoff-tasmota-http](https://github.com/ageorgios/homebridge-sonoff-tasmota-http) for some inspiration.
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Service, Characteristic;
+
 var request = require('request');
+const storage = require('node-persist');
 
 const packageJSON = require('./package.json');
 
@@ -18,6 +20,10 @@ function HomebridgeGarageDoorAccessory(log, config) {
   this.timeout = config["timeout"] || "1000"
   this.hostname = config["host"] || "tasmota"
   this.password = config["password"] || "";
+  this.useTimeslots = config["useTimeslots"] || false;
+  this.timeslotIndex = config["timeslotIndex"];
+
+  if (this.useTimeslots) initTimeslot();
 
   this.service = new Service.Switch(this.name);
 
@@ -64,7 +70,27 @@ HomebridgeGarageDoorAccessory.prototype.getState = function (callback) {
 }
 
 HomebridgeGarageDoorAccessory.prototype.setState = function (toggle, callback) {
-  var garageDoor = this
+  var garageDoor = this;
+
+  if (garageDoor.useTimeslots == true) {
+    if (garageDoor.timeslotIndex != null) {
+      timeslotIndexAndCheck(toggle, callback, garageDoor, garageDoor.timeslotIndex);
+    } else {
+      garageDoor.log("Garage-Door: Timeslot Index for " + garageDoor.name + " not set. Turning on normally.");
+      turnOnDevice(toggle, callback, garageDoor);
+    }
+  } else {
+    turnOnDevice(toggle, callback, garageDoor);
+  }
+}
+
+function timeslotIndexAndCheck(toggle, callback, garageDoor, index) {
+  setTimeout(() => {
+    timeslotCheck(toggle, callback, garageDoor)
+  }, (index * 10)); // (index * 5 * 5)
+}
+
+function turnOnDevice(toggle, callback, garageDoor) {
   var newstate = "%20Off"
   if (toggle) newstate = "%20On"
   request("http://" + garageDoor.hostname + "/cm?user=admin&password=" + encodeURIComponent(garageDoor.password) + "&cmnd=Power" + garageDoor.relay + newstate, function (error, response, body) {
@@ -85,4 +111,56 @@ HomebridgeGarageDoorAccessory.prototype.setState = function (toggle, callback) {
     }, garageDoor.timeout);
 
   })
+}
+
+async function timeslotCheck(toggle, callback, garageDoor) {
+  let isSet = await storage.getItem('isSet');
+  if (isSet != true) {
+    garageDoor.log("Garage-Door: Timeslot for " + garageDoor.name + " is available.");
+    timeslotFree(toggle, callback, garageDoor);
+  } else {
+    let endDate = await storage.getItem('endDate');
+    let currentDate = new Date();
+    if (currentDate > Date.parse(endDate)) {
+      garageDoor.log("Garage-Door: Timeslot for " + garageDoor.name + " is available.");
+      timeslotFree(toggle, callback, garageDoor);
+    } else {
+      garageDoor.log("Garage-Door: Timeslot for " + garageDoor.name + " is NOT available. Waiting.");
+      timeslotWaiting(toggle, callback, garageDoor);
+    }
+  }
+}
+
+async function timeslotWaiting(toggle, callback, garageDoor) {
+  let isSet = await storage.getItem('isSet');
+  if (isSet != true) {
+    garageDoor.log("Garage-Door: Timeslot for " + garageDoor.name + " is available.");
+    timeslotFree(toggle, callback, garageDoor);
+  } else {
+    let endDate = await storage.getItem('endDate');
+    let currentDate = new Date();
+    if (currentDate > Date.parse(endDate)) {
+      garageDoor.log("Garage-Door: Timeslot for " + garageDoor.name + " is available.");
+      timeslotFree(toggle, callback, garageDoor);
+    } else {
+      timeslotWaiting(toggle, callback, garageDoor);
+    }
+  }
+}
+
+async function timeslotFree(toggle, callback, garageDoor) {
+  await storage.setItem('isSet', true);
+  await storage.setItem('startDate', new Date());
+
+  var et = new Date();
+  et.setSeconds(et.getSeconds() + 1.5);
+
+  await storage.setItem('endDate', et);
+
+  turnOnDevice(toggle, callback, garageDoor);
+
+}
+
+async function initTimeslot() {
+  await storage.init({ dir: "plugin-persist/homebridge-tasmota-garage-door/timeslots" });
 }
